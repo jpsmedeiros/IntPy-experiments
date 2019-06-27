5e7584227eaaec05e49dcaf638216d0769a219d1
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2019-06-27 10:00:13 -0300

    Adicionando o experimento de permutações heap

---
+++ heap_permutation/Python/heap_permutation.py
@@ -0,0 +1,18 @@
+import time
+
+def heap_permutation(data, n):
+    if n == 1:
+        print(data)
+        return
+    for i in range(n):
+        heap_permutation(data, n - 1)
+        if n % 2 == 0:
+            data[i], data[n - 1] = data[n - 1], data[i]
+        else:
+            data[0], data[n - 1] = data[n - 1], data[0]
+
+data = [1, 2, 3, 4, 5, 6]
+start_time = time.time()
+heap_permutation(data, len(data))
+end_time = time.time()
+print(end_time - start_time)
